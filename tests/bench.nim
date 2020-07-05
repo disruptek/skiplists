ff1833fd04d657dbbe449b52d0a57d9a4301cffe
Andy Davidoff <andy+github@disruptek.com>
2020-07-04 23:54:34 -0400

    add bench

---
+++ tests/bench.nim
@@ -0,0 +1,42 @@
+import std/strutils
+import std/intsets
+import std/sequtils
+
+import criterion
+
+import skiplists
+
+var cfg = newDefaultConfig()
+
+benchmark cfg:
+
+  var
+    rick = toSeq(0 .. 1_000)
+
+  var q = toSkipList(rick)
+
+  var x = initIntSet()
+  for i in rick:
+    incl(x, i)
+
+  proc make_intset() {.measure.} =
+    var s = initIntSet()
+    for i in rick:
+      incl(s, i)
+
+  proc make_skiplist_seq() {.measure.} =
+    var s {.used.} = toSkipList(rick)
+
+  when false:
+    proc make_skiplist_naive() {.measure.} =
+      var s: SkipList[int]
+      for i in rick:
+        add(s, i)
+
+  proc contains_intset() {.measure.} =
+    for i in rick:
+      discard i in x
+
+  proc contains_skiplist() {.measure.} =
+    for i in rick:
+      discard i in q
